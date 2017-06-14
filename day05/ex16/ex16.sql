f0b6545d450178426a16f802d95e28666ccc30d4
alinacornea <alinacornea18@gmail.com>
2017-06-13 20:16:53 -0700

    Create ex16.sql

--- day05/ex16/ex16.sql
+++ day05/ex16/ex16.sql
@@ -1,2 +1,2 @@
 select count(id_film) as 'movies' from member_history
-	where date between 10-30-2006 and 07/27/2007 or month(date) = 12 and day(date) = 24;
+	where date between 10-30-2006 and 07-27-2007 or month(date) = 12 and day(date) = 24;
