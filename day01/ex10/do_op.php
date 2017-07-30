3927c35727109c7a8af5b88cb0c476e412fd5110
Alina CORNEA <alcornea@e1z2r9p2.42.us.org>
2017-07-30 02:04:37 -0700

    ex10

---
+++ day01/ex10/do_op.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+if ($argc != 4)
+	echo "Incorrect Parameters";
+else
+{
+  $a = trim($argv[1]);
+  $b = trim($argv[2]);
+  $c = trim($argv[3]);
+	if ($b == "+")
+		print ($a + $c);
+	if ($b == "-")
+		print ($a - $c);
+	if ($b == "*")
+		print ($a * $c);
+	if ($b == "/")
+		print ($a / $c);
+	if ($b == "%")
+		print ($a % $c);
+}
+echo "\n";
+?>
